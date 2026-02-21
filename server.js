9c066499c866cb8cf5330c689412eda6083452d0
thiarehernandezg-cloud <thiarehernandez.g@gmail.com>
2026-02-21 17:13:48 -0300

    prueba

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.post('/enviar-contacto', async (req, res) => {
 
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 465,
-        secure: true, 
+      port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
